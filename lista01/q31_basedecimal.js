eec546ea8224e99385e0624af3b0b7a493fb9987
EmanoelSousa70 <61412472+EmanoelSousa70@users.noreply.github.com>
2021-05-21 10:20:18 -0300

    Delete lista01 directory

--- lista01/q31_basedecimal.js
+++
@@ -1,6 +0,0 @@
-//. Leia um número inteiro (4 dígitos binários), calcule e escreva o equivalente na base decimal
-const input = require('prompt-sync')()
-
-//Entrada
-numero_binario = Number(input('Digite um numero binario com 4 digitos: '))
-
